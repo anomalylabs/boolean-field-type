2b30f6fda7674ec251986f7107fb4470e7277326
Denis Efremov <efremov.a.denis@gmail.com>
2017-03-15 06:50:48 +0300

    Update ajax.js

--- resources/js/ajax.js
+++ resources/js/ajax.js
@@ -1,15 +1,16 @@
 $(function () {
-
     // Initialize form switches
     $('[data-provides="ajax-switch"]').each(function () {
-        $(this).bootstrapSwitch({
+        var $this = $(this);
+
+        $this.bootstrapSwitch({
             onSwitchChange: function (e, state) {
                 $.post('/streams/boolean-field_type/toggle', {
                     state: state,
-                    entry: $(this).data('entry'),
-                    field: $(this).data('field'),
-                    stream: $(this).data('stream'),
-                    namespace: $(this).data('namespace')
+                    entry: $this.data('entry'),
+                    field: $this.data('field'),
+                    stream: $this.data('stream'),
+                    namespace: $this.data('namespace')
                 });
             }
         });
